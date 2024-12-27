136c61848ef5f7926edb81b9478a421fcc077740
Manish <msn07082005@gmail.com>
2024-12-27 16:47:47 +0530

    personal info reduced

--- lib/screens/personal_info_screen.dart
+++ lib/screens/personal_info_screen.dart
@@ -16,16 +16,10 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
   final nameController = TextEditingController();
   final ageController = TextEditingController();
   final occupationController = TextEditingController();
-  final heightController = TextEditingController();
-  final weightController = TextEditingController();
   final mobileController = TextEditingController();
-  final emailController = TextEditingController();
-  final addressController = TextEditingController();
 
   String? gender;
   bool isSelf = true;
-  bool isAthlete = false;
-  DateTime? dob;
 
   @override
   Widget build(BuildContext context) {
@@ -70,39 +64,13 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
                     value!.isEmpty ? 'Please enter name' : null,
               ),
 
-              // Date of Birth or Age
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      controller: ageController,
-                      decoration: const InputDecoration(labelText: 'Age'),
-                      keyboardType: TextInputType.number,
-                      validator: (value) =>
-                          value!.isEmpty ? 'Please enter age' : null,
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  ElevatedButton(
-                    onPressed: () async {
-                      final pickedDate = await showDatePicker(
-                        context: context,
-                        initialDate: dob ?? DateTime.now(),
-                        firstDate: DateTime(1900),
-                        lastDate: DateTime.now(),
-                      );
-                      if (pickedDate != null && pickedDate != dob) {
-                        setState(() {
-                          dob = pickedDate;
-                          ageController.text =
-                              ((DateTime.now().difference(dob!).inDays) ~/ 365)
-                                  .toString();
-                        });
-                      }
-                    },
-                    child: const Text('Select Date of Birth'),
-                  ),
-                ],
+              // Age
+              TextFormField(
+                controller: ageController,
+                decoration: const InputDecoration(labelText: 'Age'),
+                keyboardType: TextInputType.number,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter age' : null,
               ),
 
               // Gender Selection
@@ -113,8 +81,6 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
                   'Select',
                   'Male',
                   'Female',
-                  'Non-Binary',
-                  'Prefer Not to Say'
                 ]
                     .map((label) => DropdownMenuItem(
                           child: Text(label),
@@ -134,61 +100,11 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
                 validator: (value) =>
                     value!.isEmpty ? 'Please enter a mobile number' : null,
               ),
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(labelText: 'Email Address'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) =>
-                    value!.isEmpty ? 'Please enter an email address' : null,
-              ),
-              TextFormField(
-                controller: addressController,
-                decoration: const InputDecoration(labelText: 'Address'),
-                keyboardType: TextInputType.streetAddress,
-              ),
-
-              // Height and Weight
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      controller: heightController,
-                      decoration:
-                          const InputDecoration(labelText: 'Height (cm)'),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: TextFormField(
-                      controller: weightController,
-                      decoration:
-                          const InputDecoration(labelText: 'Weight (kg)'),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ],
-              ),
 
               // Occupation
               TextFormField(
                 controller: occupationController,
                 decoration: const InputDecoration(labelText: 'Occupation'),
-              ),
-
-              // Athlete Status
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    const Text(
-                        'Are you an athlete or involved in professional sports?'),
-                    Checkbox(
-                      value: isAthlete,
-                      onChanged: (value) => setState(() => isAthlete = value!),
-                    ),
-                  ],
-                ),
               ),
 
               const SizedBox(height: 20),
@@ -208,16 +124,10 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
       final personalInfo = {
         'name': nameController.text,
         'age': int.tryParse(ageController.text) ?? 0,
-        'dob': dob,
         'gender': gender,
         'occupation': occupationController.text,
         'is_self': isSelf,
-        'is_athlete': isAthlete,
         'mobile': mobileController.text,
-        'email': emailController.text,
-        'address': addressController.text,
-        'height': double.tryParse(heightController.text) ?? 0.0,
-        'weight': double.tryParse(weightController.text) ?? 0.0,
       };
 
       // Update state in provider
